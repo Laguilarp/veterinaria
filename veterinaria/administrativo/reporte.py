1f9752cab57fb78af96d715a49a82ccacc7fc49b
usuario <usuario@gmail.com>
2024-11-23 19:02:10 -0500

    Carnet de vacunación

--- veterinaria/administrativo/reporte.py
+++ veterinaria/administrativo/reporte.py
@@ -225,6 +225,28 @@ def view_reporte(request):
                 except Exception as ex:
                     return JsonResponse({"respuesta": False, "mensaje": "Error al generar el certificado."})
 
+            if peticion == 'descargarcarnet':
+                try:
+                    data['mascota'] = mascota = Mascota.objects.get(id=int(request.POST['id']))
+                    data['historiales'] = historial = HistorialMedico.objects.filter(status=True, mascota=mascota)
+                    data['fechaactual'] = datetime.now().date()
+                    data['tiporeporte'] = 'Historial médico'
+                    name = "reporte_historial" + str(mascota.id)
+                    crear_carpeta = os.path.join(os.path.join(ALMACENAMIENTO, 'media', 'reportes'))
+                    try:
+                        os.makedirs(crear_carpeta)
+                    except Exception as ex:
+                        pass
+                    valida = convertir_html_a_pdf_reporte(
+                        'reportes/historialvacunacion.html',
+                        {'pagesize': 'A4', 'data': data, 'MEDIA_ROOT': MEDIA_ROOT}, name + '.pdf'
+                    )
+                    if valida:
+                        return JsonResponse({"respuesta": True, "mensaje": "Reporte generado correctamente.", "name": name + '.pdf'})
+                    return JsonResponse({"respuesta": False, "mensaje": "Error al generar el certificado."})
+                except Exception as ex:
+                    return JsonResponse({"respuesta": False, "mensaje": "Error al generar el certificado."})
+
             if peticion == 'generarhistorial':
                 try:
                     data['propietario'] = propietario = Propietario.objects.filter(status=True, persona__documento=request.POST['respuesta'], persona__status=True).first()
